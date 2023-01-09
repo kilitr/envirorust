6c1cdc2ef14ce03296b5dc874b9a6b23316c9c81
Kilian Trosien <1645037+kilitr@users.noreply.github.com>
2023-01-09 18:14:32 +0100

    Push data into influx

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use core::time;
 use std::thread;
 
-use rppal::hal as hal;
-use rppal::i2c::I2c;
-use bme280::{BME280, Measurements};
-use ltr_559::{Ltr559, SlaveAddr, AlsIntTime, AlsMeasRate, AlsGain};
+use dotenv::dotenv;
+
+use bme280::{Measurements, BME280};
+use ltr_559::{AlsGain, AlsIntTime, AlsMeasRate, Ltr559, SlaveAddr};
+use rppal::{hal, i2c::I2c};
+
+use futures::prelude::*;
+use influxdb2::models::DataPoint;
+use influxdb2::Client;
 
 fn bme_measurements() -> Measurements<rppal::i2c::Error> {
     let i2c_bus = I2c::new().expect("Error during i2c initialization");
@@ -25,11 +30,56 @@ fn ltr_measurements() -> f32 {
     value
 }
 
-fn main() {
+struct RoomClimateReading {
+    temperature: f64,
+    humidity: f64,
+    pressure: f64,
+    light: f64,
+}
+
+async fn write_measurement(cr: RoomClimateReading) -> Result<(), Box<dyn std::error::Error>> {
+    let host = std::env::var("INFLUXDB_HOST").unwrap();
+    let org = std::env::var("INFLUXDB_ORG").unwrap();
+    let token = std::env::var("INFLUXDB_TOKEN").unwrap();
+    let bucket = std::env::var("INFLUXDB_BUCKET").unwrap();
+    let client = Client::new(host, org, token);
+
+    let points = vec![DataPoint::builder("room")
+        .tag("room", "Kilian")
+        .field("temperature", cr.temperature)
+        .field("humidity", cr.humidity)
+        .field("pressure", cr.pressure)
+        .field("light", cr.light)
+        .build()?];
+    client.write(bucket.as_str(), stream::iter(points)).await?;
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
     loop {
         let thp = bme_measurements();
         let light = ltr_measurements();
-        println!("{}°C {}% {}pascal {}lux", thp.temperature, thp.humidity, thp.pressure, light);
-        thread::sleep(time::Duration::from_secs(1));
+
+        println!(
+            "{}°C {}% {}pascal {}lux",
+            thp.temperature, thp.humidity, thp.pressure, light
+        );
+
+        let room_reading = RoomClimateReading {
+            temperature: thp.temperature.into(),
+            humidity: thp.humidity.into(),
+            pressure: thp.pressure.into(),
+            light: light.into(),
+        };
+
+        tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap()
+            .block_on(write_measurement(room_reading))
+            .unwrap();
+
+        thread::sleep(time::Duration::from_secs(30));
     }
 }
